9846fe7c08eb744fa5aa0ed046fd143a642a4adb
nina <nina@localhost>
2024-11-14 11:30:27 +0100

    testCarafe

--- src/atelier05/carafe.java
+++ src/atelier05/carafe.java
@@ -2,47 +2,58 @@ package atelier05;
 
 public class carafe {
 	private int contenu_, capacite_;
-	
-	/** création d'une carafe vide
-	 * @param capacite - capacité en L de la carafe */
+
 	
 	public carafe(int capacite) {
 		this.capacite_ = capacite;
 		Vider();
+		/** création d'une carafe vide
+		 * @param capacite - capacité en L de la carafe */
 	}
 	
 	public void Remplir() {
 		this.contenu_ = capacite_;
+		/** remplir une carafe pleine */
 	}
-	
-	/** remplir une carafe pleine */
 	
 	public void Vider() {
 		this.contenu_ = 0;
+		/** vider complètement une carafe */
 	}
-	
-	/** vider complètement une carafe */
 	
 	public int Contenu() {
 		return this.contenu_;
+		/** accesseur en lecture
+		 * @return contenu de la carafe en L */
 	}
 	
-	/** accesseur en lecture
-	 * @return contenu de la carafe en L */
+	private void Contenu(int contenu) {
+		this.contenu_ = contenu;
+		/** accesseur en écriture privé
+		 * @contenu de la carafe en L à modifier */
+	}
+	
+	
 	
 	public int Capacité() {
 		return this.capacite_;
-	}
-	
-	/** accesseur en lecture
-	 * @return capacité de la carafe en L */
-	
-	public void Transvaser(carafe c) {
-		this.contenu_ = capacite_;
-		c.contenu_ = c.capacite_ - this.capacite_;
-		
+		/** accesseur en lecture
+		 * @return capacité de la carafe en L */
 	}
 
+	public void Transvaser(carafe c) {
+		int reste = this.capacite_ - this.contenu_;
+		System.out.println("reste : " + reste);
+		if (c.Contenu() <= reste) { // si le contenu est inf à reste, on peut vider entièrement la carafe c
+			this.contenu_ += c.contenu_;
+			c.Vider();
+		}
+		else {
+			c.Contenu(c.Contenu() - reste);
+			this.Remplir();
+		}
 	/** transvaser la carafe c dans la carafe this ou inversement
 	 * @param c carafe à transvaser */
+	}
+
 }
--- src/atelier05/testCarafe.java
+++ src/atelier05/testCarafe.java
@@ -2,12 +2,18 @@ package atelier05;
 
 public class testCarafe {
 	public static void main(String[] args) {
-		carafe c1 = new carafe(5);
+		carafe c1 = new carafe(4);
 		carafe c2 = new carafe(7);
 		
 		System.out.println("capcité de la carafe 1 : " + c1.Capacité());
 		System.out.println("capcité de la carafe 2 : " + c2.Capacité());
 		
+		System.out.println("contenu de la carafe 1 : " + c1.Contenu());
+		System.out.println("contenu de la carafe 2 : " + c2.Contenu());
+		
+		c2.Remplir();
+		System.out.println("contenu de la carafe 1 : " + c1.Contenu());
+		c1.Transvaser(c2);
 		System.out.println("contenu de la carafe 1 : " + c1.Contenu());
 		System.out.println("contenu de la carafe 2 : " + c2.Contenu());
 	}
